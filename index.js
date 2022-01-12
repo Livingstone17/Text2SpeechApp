57c041201a9a25df81f3b8303f00c0882ee6f1ef
Livingstone17 <peterson.omobolaji@gmail.com>
2022-01-13 00:54:04 +0100

    set up project with methods

---
+++ index.js
@@ -0,0 +1,101 @@
+
+// var voiceList = document.querySelector('#voiceList');
+// var textInput = document.querySelector('#txtInput');
+// var btnSpeak = document.querySelector('#btnSpeak');
+// var txt2Speech = window.speechSynthesis;
+// var voices = [];
+// const GetVoices = () =>{
+//     voices= txt2Speech.getVoices();
+
+//     voiceList.innerHTML = '';
+//     voices.forEach((voice)=>{
+//         var listItem = document.createElement('option');
+//         listItem.textContent = voice.name;
+//         listItem.setAttribute('data-lang',voice.lang);
+//         listItem.setAttribute('data-name',voice.name);
+//         voiceList.appendChild(listItem);
+//     });
+//     voiceList.selectedIndex = 0 
+// }
+// if(speechSynthesis !== undefined){
+//     speechSynthesis.onvoiceschanged=GetVoices;
+// }
+// btnSpeak.addEventListener('click', ()=>{
+//     var toSpeak= new  SpeechSynthesisUtterance(textInput.value);
+//     var selectedVoiceName =  voiceList.selectedOptions[0].getAttribute('data-name');
+//     voices.forEach((voice)=>{
+//         if(voice.name===selectedVoiceName){
+//             toSpeak.voice = voice;
+//         }
+//     });
+//     txt2Speech.speak(toSpeak);
+// })
+// GetVoices();
+
+speechSynthesis.cancel();
+
+var isSpeaking = false;
+
+var speech = new SpeechSynthesisUtterance();
+speech.rate =1;
+speech.pitch=1;
+speech.volume=1;
+speech.voice = speechSynthesis.getVoices()[0];
+
+const speakText = () =>{
+    isSpeaking =true;
+    speech.text=document.querySelector('#textInput').value;
+    speechSynthesis.speak(speech);
+}
+
+// method to pause speech
+pauseSpeech = () =>{
+    if(isSpeaking){
+        isSpeaking=false;
+        speechSynthesis.pause();
+        document.getElementById('btnPause').innerHTML = `Resume`
+    }else{
+        isSpeaking = true;
+        speechSynthesis.resume();
+        document.getElementById('btnPause').innerHTML = `Pause`
+    }
+}
+
+// method to stop speech
+stopSpeech = ()=>{
+    isSpeaking =false;
+    speechSynthesis.cancel();
+}
+
+// method to change the voices
+changeVoice = (voice) =>{
+    if(voice == 'voice1'){
+        speech.voice = speechSynthesis.getVoices()[8];
+    }else if(voice == 'voice7'){
+        speech.voice = speechSynthesis.getVoices()[33];
+    }else if(voice == 'voice2'){
+        speech.voice = speechSynthesis.getVoices()[0];
+    }else if(voice == 'voice9'){
+        speech.voice = speechSynthesis.getVoices()[41];
+    }
+}
+
+
+changeVoiceSpeed=(voiceSpeed)=> {
+    // For some reason, speed below 0.5 doesn't work
+    if (voiceSpeed == "speed2") {
+      speech.rate = 2;
+    } else if (voiceSpeed == "speed1.75") {
+      speech.rate = 1.75;
+    } else if (voiceSpeed == "speed1.5") {
+      speech.rate = 1.5;
+    } else if (voiceSpeed == "speed1.25") {
+      speech.rate = 1.25;
+    } else if (voiceSpeed == "speed1") {
+      speech.rate = 1;
+    } else if (voiceSpeed == "speed0.75") {
+      speech.rate = 0.75;
+    } else if (voiceSpeed == "speed0.5") {
+      speech.rate = 0.5;
+    }
+  }
